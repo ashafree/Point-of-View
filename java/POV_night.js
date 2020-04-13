66c1e3a7f2a71565bc980d8111294e40f54c28be
ashafree <kima601@newschool.edu>
2020-04-13 12:08:06 -0400

    new

---
+++ java/POV_night.js
@@ -0,0 +1,71 @@
+ // var clickMeButton = document.querySelector(".who");
+
+// var clickWho = document.querySelector(".who");
+// var whoPicture = document.querySelector(".whopic");
+
+// clickWho.addEventListener("click", function(){ whoPicture.classList.toggle("appear");
+// })
+
+var clickMeButton = document.querySelector(".one");
+
+var clickOne = document.querySelector(".one");
+var onePicture = document.querySelector(".onepic");
+
+clickOne.addEventListener("click", function(){ onePicture.classList.toggle("appear");
+})
+
+var clickMeButton = document.querySelector(".two");
+
+var clickTwo = document.querySelector(".two");
+var twoPicture = document.querySelector(".twopic");
+
+clickTwo.addEventListener("click", function(){ twoPicture.classList.toggle("appear");
+})
+
+var clickMeButton = document.querySelector(".three");
+
+var clickThree = document.querySelector(".three");
+var threePicture = document.querySelector(".threepic");
+
+clickThree.addEventListener("click", function(){ threePicture.classList.toggle("appear");
+})
+
+var clickMeButton = document.querySelector(".four");
+
+var clickFour = document.querySelector(".four");
+var fourPicture = document.querySelector(".fourpic");
+
+clickFour.addEventListener("click", function(){ fourPicture.classList.toggle("appear");
+})
+
+var clickMeButton = document.querySelector(".five");
+
+var clickFive = document.querySelector(".five");
+var fivePicture = document.querySelector(".fivepic");
+
+clickFive.addEventListener("click", function(){ fivePicture.classList.toggle("appear");
+})
+
+var clickMeButton = document.querySelector(".six");
+
+var clickSix = document.querySelector(".six");
+var sixPicture = document.querySelector(".sixpic");
+
+clickSix.addEventListener("click", function(){ sixPicture.classList.toggle("appear");
+})
+
+var clickMeButton = document.querySelector(".seven");
+
+var clickSeven = document.querySelector(".seven");
+var sevenPicture = document.querySelector(".sevenpic");
+
+clickSeven.addEventListener("click", function(){ sevenPicture.classList.toggle("appear");
+})
+
+var clickMeButton = document.querySelector(".eight");
+
+var clickEight = document.querySelector(".eight");
+var eightPicture = document.querySelector(".eightpic");
+
+clickEight.addEventListener("click", function(){ eightPicture.classList.toggle("appear");
+})
